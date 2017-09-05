71533a66e950018563635efee3a61dfcc6905e47
Kyle Atmore <atmorek@gmail.com>
2017-09-05 10:39:16 -0400

    Fix Session class to properly update cookie

--- lib/session.rb
+++ lib/session.rb
@@ -1,23 +1,19 @@
 require 'json'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
     cookie = req.cookies['_rails_lite_app']
     @cookie_val = cookie ? JSON.parse(cookie) : {}
   end
 
   def [](key)
-    @cookie_val[key.to_sym]
+    @cookie_val[key]
   end
 
   def []=(key, val)
-    @cookie_val[key.to_sym] = val.to_sym
+    @cookie_val[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     res.set_cookie('_rails_lite_app', {
       value: @cookie_val.to_json,
