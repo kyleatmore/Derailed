e335919b9d3e6be0570b524a339998558d45453b
Kyle Atmore <atmorek@gmail.com>
2017-09-05 15:32:18 -0400

    Create exception middleware and exception view template

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,31 @@
 require 'erb'
+require 'rack'
+require 'byebug'
 
 class ShowExceptions
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    @error = e
+    template_file = File.read("lib/templates/rescue.html.erb")
+    template = ERB.new(template_file).result(binding)
+
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write(template)
+    res.finish
   end
 
 end
